8e7fb713d216fee7d8685c87f83be9dbd4494afa
supriyakupekar1999-lgtm <supriyakupekar1999@gmail.com>
2026-06-21 20:39:45 +0530

    Refactor LoginPage_POM for improved readability

--- src/test/java/POM_Pages/LoginPage_POM.java
+++ src/test/java/POM_Pages/LoginPage_POM.java
@@ -10,22 +10,31 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import helper.BasePage;
 
+
 public class LoginPage_POM extends BasePage {
+
 
     WebDriverWait wait;
 
+
     public LoginPage_POM(WebDriver driver) {
+
         super(driver);
+
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
     }
+
 
 
     @FindBy(name = "username")
     private WebElement username;
 
 
+
     @FindBy(name = "password")
     private WebElement password;
+
 
 
     @FindBy(name = "signon")
@@ -33,41 +42,82 @@ public class LoginPage_POM extends BasePage {
 
 
 
+
     public void setUsername(String user) {
 
-        wait.until(ExpectedConditions.visibilityOf(username));
+
+        wait.until(
+                ExpectedConditions.visibilityOf(username)
+        );
+
 
         username.clear();
+
         username.sendKeys(user);
+
     }
+
+
 
 
 
     public void setPassword(String pass) {
 
+
+        wait.until(
+                ExpectedConditions.visibilityOf(password)
+        );
+
+
         password.clear();
+
         password.sendKeys(pass);
+
     }
+
+
 
 
 
     public void clickSignOn() {
 
+
         wait.until(
+
                 ExpectedConditions.elementToBeClickable(loginBtn)
+
         ).click();
 
+
     }
 
 
 
-    // your old test also uses this
+
+
     public void login(String user, String pass) {
 
+
         setUsername(user);
+
+
         setPassword(pass);
+
+
         clickSignOn();
 
+
+
+        // wait until login completes and catalog page loads
+
+        wait.until(
+
+                ExpectedConditions.urlContains("Catalog")
+
+        );
+
+
     }
+
 
 }
