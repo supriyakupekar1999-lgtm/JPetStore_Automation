6392ff8c758d66361f5e07754c4c8389b66e613c
supriyakupekar1999-lgtm <supriyakupekar1999@gmail.com>
2026-06-21 19:56:35 +0530

    Update HomePage_POM.java

--- src/test/java/POM_Pages/HomePage_POM.java
+++ src/test/java/POM_Pages/HomePage_POM.java
@@ -2,7 +2,6 @@ package POM_Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
 import helper.BasePage;
 
 public class HomePage_POM extends BasePage {
@@ -11,58 +10,16 @@ public class HomePage_POM extends BasePage {
         super(driver);
     }
 
+
     public void clickFish() {
 
         System.out.println("Inside clickFish");
 
         driver.findElement(
-                By.xpath("//img[contains(@src,'sm_fish.gif')]"))
-                .click();
+                By.xpath("//img[contains(@src,'sm_fish.gif')]")
+        ).click();
 
         System.out.println("Fish clicked");
     }
 
-    public void clickDogs() {
-
-        System.out.println("Inside clickDogs");
-
-        driver.findElement(
-                By.xpath("//img[contains(@src,'sm_dogs.gif')]"))
-                .click();
-
-        System.out.println("Dogs clicked");
-    }
-
-    public void clickReptiles() {
-
-        System.out.println("Inside clickReptiles");
-
-        driver.findElement(
-                By.xpath("//img[contains(@src,'sm_reptiles.gif')]"))
-                .click();
-
-        System.out.println("Reptiles clicked");
-    }
-
-    public void clickCats() {
-
-        System.out.println("Inside clickCats");
-
-        driver.findElement(
-                By.xpath("//img[contains(@src,'sm_cats.gif')]"))
-                .click();
-
-        System.out.println("Cats clicked");
-    }
-
-    public void clickBirds() {
-
-        System.out.println("Inside clickBirds");
-
-        driver.findElement(
-                By.xpath("//img[contains(@src,'sm_birds.gif')]"))
-                .click();
-
-        System.out.println("Birds clicked");
-    }
 }
