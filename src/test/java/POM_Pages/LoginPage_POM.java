e25fbcbdf07df0fc33fd22778a4c418797d1ec66
supriyakupekar1999-lgtm <supriyakupekar1999@gmail.com>
2026-06-21 21:53:07 +0530

    Update LoginPage_POM.java

--- src/test/java/POM_Pages/LoginPage_POM.java
+++ src/test/java/POM_Pages/LoginPage_POM.java
@@ -2,6 +2,7 @@ package POM_Pages;
 
 import java.time.Duration;
 
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,61 +13,35 @@ import helper.BasePage;
 
 public class LoginPage_POM extends BasePage {
 
-
     WebDriverWait wait;
 
-
     public LoginPage_POM(WebDriver driver) {
-
         super(driver);
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
-
-    @FindBy(name="username")
+    @FindBy(name = "username")
     private WebElement username;
 
-
-    @FindBy(name="password")
+    @FindBy(name = "password")
     private WebElement password;
 
-
-    @FindBy(name="signon")
+    @FindBy(name = "signon")
     private WebElement loginBtn;
 
+    public void login(String user, String pass) {
 
-
-    public void login(String user,String pass){
-
-
-        wait.until(
-                ExpectedConditions.visibilityOf(username)
-        );
-
+        wait.until(ExpectedConditions.visibilityOf(username));
 
         username.clear();
         username.sendKeys(user);
 
-
         password.clear();
         password.sendKeys(pass);
 
-
-
-        wait.until(
-                ExpectedConditions.elementToBeClickable(loginBtn)
-        ).click();
-
-
-
-        wait.until(
-                ExpectedConditions.urlContains("Catalog.action")
-        );
-
+        loginBtn.click();
 
     }
-
-
 }
+
+    
