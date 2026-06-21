dd98d015ff1683fe34f31e020ba93a767b4a3692
supriyakupekar1999-lgtm <supriyakupekar1999@gmail.com>
2026-06-21 20:38:10 +0530

    Update HomePage_POM.java

--- src/test/java/POM_Pages/HomePage_POM.java
+++ src/test/java/POM_Pages/HomePage_POM.java
@@ -1,74 +1,100 @@
 package POM_Pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import helper.BasePage;
 
 public class HomePage_POM extends BasePage {
 
+
+    WebDriverWait wait;
+
+
     public HomePage_POM(WebDriver driver) {
+
         super(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
     }
+
 
 
     public void clickFish() {
 
-        System.out.println("Inside clickFish");
 
-        driver.findElement(
+        wait.until(
+            ExpectedConditions.elementToBeClickable(
                 By.xpath("//img[contains(@src,'sm_fish.gif')]")
+            )
         ).click();
 
-        System.out.println("Fish clicked");
+
     }
+
+
 
 
     public void clickDogs() {
 
-        System.out.println("Inside clickDogs");
 
-        driver.findElement(
+        wait.until(
+            ExpectedConditions.elementToBeClickable(
                 By.xpath("//img[contains(@src,'sm_dogs.gif')]")
+            )
         ).click();
 
-        System.out.println("Dogs clicked");
+
     }
+
+
 
 
     public void clickCats() {
 
-        System.out.println("Inside clickCats");
 
-        driver.findElement(
+        wait.until(
+            ExpectedConditions.elementToBeClickable(
                 By.xpath("//img[contains(@src,'sm_cats.gif')]")
+            )
         ).click();
 
-        System.out.println("Cats clicked");
+
     }
+
+
+
 
 
     public void clickBirds() {
 
-        System.out.println("Inside clickBirds");
 
-        driver.findElement(
+        wait.until(
+            ExpectedConditions.elementToBeClickable(
                 By.xpath("//img[contains(@src,'sm_birds.gif')]")
+            )
         ).click();
 
-        System.out.println("Birds clicked");
+
     }
+
+
 
 
     public void clickReptiles() {
 
-        System.out.println("Inside clickReptiles");
 
-        driver.findElement(
+        wait.until(
+            ExpectedConditions.elementToBeClickable(
                 By.xpath("//img[contains(@src,'sm_reptiles.gif')]")
+            )
         ).click();
 
-        System.out.println("Reptiles clicked");
+
     }
 
 }
