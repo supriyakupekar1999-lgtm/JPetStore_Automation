fd119c6170f63dcd8f720104fb6bc6205b4b5ffa
supriyakupekar1999-lgtm <supriyakupekar1999@gmail.com>
2026-06-21 21:53:24 +0530

    Update ProductPage_POM.java

--- src/test/java/POM_Pages/ProductPage_POM.java
+++ src/test/java/POM_Pages/ProductPage_POM.java
@@ -3,76 +3,32 @@ package POM_Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import helper.BasePage;
 
 public class ProductPage_POM extends BasePage {
 
-
-    WebDriver driver;
-
-
-    public ProductPage_POM(WebDriver wd) {
-        super(wd);
-        this.driver = wd;
+    public ProductPage_POM(WebDriver driver) {
+        super(driver);
     }
 
+    @FindBy(xpath = "//*[@id='Catalog']//a[text()='FI-SW-01']")
+    private WebElement Angelfish;
 
+    @FindBy(xpath = "//*[@id='Catalog']//a[text()='FI-SW-02']")
+    private WebElement tigerShark;
+    
+    @FindBy(xpath = "//*[@id='Catalog']//a[text()='K9-BD-01']")
+    private WebElement Bulldog;
+    
 
-    @FindBy(xpath="//a[contains(text(),'FI-SW-01')]")
-    WebElement angelfish;
-
-
-
-    @FindBy(xpath="//a[contains(text(),'FI-SW-02')]")
-    WebElement tigerShark;
-
-
-
-    @FindBy(xpath="//a[contains(text(),'K9-BD-01')]")
-    WebElement bulldog;
-
-
-
-
-    public void selectAngelfish()
-    {
-        waitForElement(angelfish);
-        angelfish.click();
+    public void selectAngelfish() {
+    	Angelfish.click();
     }
 
-
-
-    public void selectTigerShark()
-    {
-        waitForElement(tigerShark);
+    public void selectTigerShark() {
         tigerShark.click();
     }
-
-
-
-    public void selectBulldog()
-    {
-        waitForElement(bulldog);
-        bulldog.click();
+    public void selectBulldog() {
+    	Bulldog.click();
     }
-
-
-
-    public void waitForElement(WebElement element)
-    {
-        WebDriverWait wait =
-                new WebDriverWait(
-                        driver,
-                        java.time.Duration.ofSeconds(20)
-                );
-
-
-        wait.until(
-                ExpectedConditions.elementToBeClickable(element)
-        );
-    }
-
 }
