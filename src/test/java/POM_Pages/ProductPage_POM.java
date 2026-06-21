7f39c7e02c0452c9d9c3c97721da51151067d471
supriyakupekar1999-lgtm <supriyakupekar1999@gmail.com>
2026-06-21 20:45:55 +0530

    Update ProductPage_POM.java

--- src/test/java/POM_Pages/ProductPage_POM.java
+++ src/test/java/POM_Pages/ProductPage_POM.java
@@ -3,75 +3,81 @@ package POM_Pages;
 
 import java.time.Duration;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import helper.BasePage;
 
 
-
 public class ProductPage_POM extends BasePage {
 
 
-    WebDriverWait wait;
-
-
-    public ProductPage_POM(WebDriver driver) {
-
-        super(driver);
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-
-    }
+WebDriverWait wait;
 
 
 
-    public void selectAngelfish() {
+public ProductPage_POM(WebDriver driver){
 
+super(driver);
 
-        wait.until(
-            ExpectedConditions.elementToBeClickable(
-                By.xpath("//a[contains(text(),'FI-SW-01')]")
-            )
-        ).click();
+wait=new WebDriverWait(driver,Duration.ofSeconds(20));
 
-
-    }
+}
 
 
 
-
-    public void selectTigerShark() {
-
-
-        wait.until(
-            ExpectedConditions.elementToBeClickable(
-                By.xpath("//a[contains(text(),'FI-SW-02')]")
-            )
-        ).click();
+@FindBy(xpath="//a[text()='FI-SW-01']")
+WebElement Angelfish;
 
 
-    }
+
+@FindBy(xpath="//a[text()='FI-SW-02']")
+WebElement tigerShark;
+
+
+
+@FindBy(xpath="//a[text()='K9-BD-01']")
+WebElement Bulldog;
 
 
 
 
-
-    public void selectBulldog() {
-
-
-        wait.until(
-            ExpectedConditions.elementToBeClickable(
-                By.xpath("//a[contains(text(),'K9-BD-01')]")
-            )
-        ).click();
+public void selectAngelfish(){
 
 
-    }
+wait.until(
+ExpectedConditions.elementToBeClickable(Angelfish)
+).click();
 
+
+}
+
+
+
+public void selectTigerShark(){
+
+
+wait.until(
+ExpectedConditions.elementToBeClickable(tigerShark)
+).click();
+
+
+}
+
+
+
+public void selectBulldog(){
+
+
+wait.until(
+ExpectedConditions.elementToBeClickable(Bulldog)
+).click();
+
+
+}
 
 
 }
